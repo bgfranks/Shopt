a1e1664dff6af880532f93ba863b0af54b33d63b
Brandon Franks <bgfranks.dev@gmail.com>
2022-08-21 16:11:34 -0400

    created the nav and the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,13 @@
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+
 export default function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className='footer-container'>
+      <p>&copy;2022 Shopt All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
 }
