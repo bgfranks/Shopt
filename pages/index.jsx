4125f3b3c779feb30b308654d46cd7a4e829af4a
Brandon Franks <bgfranks.dev@gmail.com>
2022-08-20 21:03:07 -0400

    linked sanity client to pull data to the herobanner

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,30 @@
 import { FooterBanner, Product, HeroBanner } from '../components'
+import { client } from '../Lib/client'
 
-export default function Home() {
+export default function Home({ products, bannerData }) {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {['product 1', 'product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData },
+  }
 }
