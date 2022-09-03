a540a3fa4fa5575e855c3f404695ebbff58627ec
Brandon Franks <bgfranks.dev@gmail.com>
2022-09-03 00:07:07 -0400

    created the success page after checkout

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
             quantity: item.quantity,
           }
         }),
-        success_url: `${req.headers.origin}/`,
+        success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/canceled`,
       }
 
